791d527dd8add68aedeb8e7a0f452845df6f52ba
roach <tmdgusya@naver.com>
2022-02-24 19:37:18 +0900

    init

---
+++ src/main/java/com/roach/autocompletionexample/AutoCompletionExampleApplication.java
@@ -0,0 +1,13 @@
+package com.roach.autocompletionexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoCompletionExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoCompletionExampleApplication.class, args);
+	}
+
+}
