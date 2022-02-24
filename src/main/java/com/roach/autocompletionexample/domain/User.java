b959903538b1b3ab5719e2976dc992e148f037e8
roach <tmdgusya@naver.com>
2022-02-24 20:34:34 +0900

    User Entity 생성

---
+++ src/main/java/com/roach/autocompletionexample/domain/User.java
@@ -0,0 +1,47 @@
+package com.roach.autocompletionexample.domain;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "email", length = 30)
+    private String email;
+
+    @Column(name = "name", length = 20)
+    private String name;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
